1912593018421ce14101a8ac29e713c86c992e98
Matt Lemke <mattrlemke@gmail.com>
2022-09-23 13:48:04 -0500

    first intro to state

--- react-complete-guide/src/Components/ExpenseItem.js
+++ react-complete-guide/src/Components/ExpenseItem.js
@@ -1,12 +1,27 @@
-function ExpenseItem() {
+import React, { useState } from 'react';
+import './ComponentCSS/ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+
+
+function ExpenseItem(props) {
+
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+   setTitle('Updated!');
+  }
+
     return (
-        <div>
-            <div>March 28 2021</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
-            </div>
+        <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
+      </Card>
     )
 }
 
